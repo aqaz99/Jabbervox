30be773493c36aabf61f1b5569832226d4099a2f
aqaz99 <jminerswebb@gmail.com>
2022-02-16 21:33:29 -0800

    Got auto transcript and snippeting working, need to expand it more

---
+++ wav_youtube/chop_wav.py
@@ -0,0 +1,51 @@
+from __future__ import unicode_literals
+import time
+from pydub import AudioSegment
+import youtube_dl
+
+# This takes about the same time as the online converter
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '192'
+    }],
+    'postprocessor_args': [
+        '-ar', '16000'
+    ],
+    'prefer_ffmpeg': True,
+    'keepvideo': False,
+    'outtmpl': 'youtube_video'
+}
+
+# Downloading a 9.5 minute video takes aroun ~2 minutes
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['http://www.youtube.com/watch?v=B15Sd0QiVzw'])
+
+# # Sleep for a few seconds to allow file to finalize
+time.sleep(3)
+
+# Examples of output from parsing transcript
+# MDCM-1|his body is disintegrating he is not of this world and the strain of being here is killing him he is not like us he is Start: 0.0 Duration: 4.38
+# MDCM-2|not one of us he can do amazing things he has one chance to return to his people if he can make it to the Start: 6.96 Duration: 5.4
+# MDCM-3|rendezvous coordinates at the appointed time he will live he will leave us and return to the world he belongs him if he Start: 13.74 Duration: 4.14
+# MDCM-4|fails he dies there are humans who care about him aid him but there are other humans chasing him they want to use him Start: 20.369 Duration: 5.49
+
+t1 = 0.0 #Works in milliseconds
+t2 = 6.96 * 1000
+t3 = 13.74 * 1000
+t4 = 20.369 * 1000 
+newAudio = AudioSegment.from_wav("wav") # wav is filename here
+# First - This is for testing
+newAudio_temp = newAudio[t1:t2]
+newAudio_temp.export('chopped1.wav', format="wav") #Exports to a wav file in the current path.
+# Second
+newAudio_temp = newAudio[t2:t3]
+newAudio_temp.export('chopped2.wav', format="wav") #Exports to a wav file in the current path.
+# Third
+newAudio_temp = newAudio[t3:t4]
+newAudio_temp.export('chopped3.wav', format="wav") #Exports to a wav file in the current path.
+# # Fourth
+# newAudio = newAudio[t1:t2]
+# newAudio.export('chopped.wav', format="wav") #Exports to a wav file in the current path.
--- wav_youtube/get_youtube_transcript.py
+++ wav_youtube/get_youtube_transcript.py
@@ -4,8 +4,20 @@ from youtube_transcript_api import YouTubeTranscriptApi
 # Exaple of returned json 
 #  {'text': "worried and desperate he hasn't given", 'start': 278.78, 'duration': 3.84}
 my_script = YouTubeTranscriptApi.get_transcript("B15Sd0QiVzw")
-index = 0
-for item in my_script:
-    to_print = "MDCM-{}|{}".format(index, item["text"])
-    print(to_print, item["start"])
-    index += 1
+counter = 0
+label = 0
+
+script_len=len(my_script)
+# print(script_len)
+for index, item in enumerate(my_script):
+    # Combine every other line of transcript to bring down count of individual wav files needed
+    if(counter % 3 == 0):
+        label+=1
+        to_print = "MDCM-{}|{} {} {}".format(
+            label, 
+            my_script[counter]["text"], 
+            (my_script[counter+1]["text"] if counter+1 < script_len else ""), # Ternary operators to not index past length of my_script
+            (my_script[counter+2]["text"] if counter+2 < script_len else "")
+            )
+        print(to_print,"Start:", item["start"], "Duration:", item["duration"])
+    counter+=1
