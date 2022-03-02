44c6edd2719603f3d3bfb713cb47d8837e36fd7f
aqaz99 <jminerswebb@gmail.com>
2022-03-01 20:59:25 -0800

    Changed gitignore, sample for dynamic audio splitting

--- wav_youtube/recognize_speech.py
+++ wav_youtube/recognize_speech.py
@@ -1,12 +1,23 @@
 # Speech recognizer python test
+# Links https://realpython.com/python-speech-recognition/
+# Also may want to check out aubio
+# https://stackoverflow.com/questions/40896370/detecting-the-index-of-silence-from-a-given-audio-file-using-python
+from pydub import AudioSegment, silence
 import speech_recognition as sr
 r = sr.Recognizer() 
-audio_file = sr.AudioFile('./wavs/MCDM-0.wav')
+audio_file = sr.AudioFile('testing.wav')
 
 with audio_file as source:
     recognized = r.record(source)
 
 print(r.recognize_google(recognized))
+
+myaudio = AudioSegment.from_wav('testing.wav')
+
+silence = silence.detect_silence(myaudio, min_silence_len=400, silence_thresh = -18)
+silence = [((start/1000),(stop/1000)) for start,stop in silence] #convert to sec
+
+print(silence)
 
 """
 Capturing Segments With offset and duration
