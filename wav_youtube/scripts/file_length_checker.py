30d62e5bda307109e5850c2dcc7d52480c373689
aqaz99 <jminerswebb@gmail.com>
2022-03-13 21:58:00 -0700

    New script, removed audio above 16s and below 2s

--- wav_youtube/scripts/file_length_checker.py
+++ wav_youtube/scripts/file_length_checker.py
@@ -10,8 +10,8 @@ def natural_sort(l):
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)] 
     return sorted(l, key=alphanum_key)
 
-
-directory = '/home/aqaz/Desktop/Jabbervox/training_data/BKOB'
+speaker = 'HLCL'
+directory = '/home/aqaz/Desktop/Jabbervox/training_data/' + speaker
 
 count = 0
 to_trim = []
@@ -19,14 +19,15 @@ for filename in natural_sort(os.listdir(directory+'/wavs')):
     full_file_name = directory+'/wavs/'+filename
     audio_file = AudioSegment.from_wav(full_file_name)
 
-    if(audio_file.duration_seconds > 16):
+    my_duration = audio_file.duration_seconds
+    if(my_duration > 16 or my_duration < 2):
         count += 1
-        # print(filename, "seconds:",audio_file.duration_seconds)
         to_trim.append(filename.split('.')[0])
-        print("Removing file:",filename)
+        print("Removing file:{}\nLength: {}".format(filename, my_duration))
         os.system('rm {}'.format(full_file_name))
 
-with open('/home/aqaz/Desktop/Jabbervox/training_data/BKOB/metadata.csv') as f:
+
+with open('/home/aqaz/Desktop/Jabbervox/training_data/{}/metadata.csv'.format(speaker)) as f:
     data = f.readlines()
 
 for index, line in enumerate(data):
@@ -35,7 +36,7 @@ for index, line in enumerate(data):
             print("Removing line {} from file".format(index))
             data[index] = ""
 
-with open('/home/aqaz/Desktop/Jabbervox/training_data/BKOB/metadata.csv', 'w', encoding='utf-8') as file:
+with open('/home/aqaz/Desktop/Jabbervox/training_data/{}/metadata.csv'.format(speaker), 'w', encoding='utf-8') as file:
     file.writelines(data)
             
 print("Total above 16 seconds:",count)
