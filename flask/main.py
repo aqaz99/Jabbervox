78777e403ec8aca65fd81ff5469d0c775d4649f1
aqaz99 <jminerswebb@gmail.com>
2022-04-11 14:49:42 -0700

    IT FINALLY WORKSgit add .!

--- flask/main.py
+++ flask/main.py
@@ -105,6 +105,6 @@ def gitPull():
     return make_response("Got changes",200, headers)
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0")
+    app.run(debug=True,host="0.0.0.0", port=8080)
 
 application = app
